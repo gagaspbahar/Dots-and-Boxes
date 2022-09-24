926cd9f000be777c6035affab982520c6be2b10c
Ubaidillah Ariq <ubaidillahariq.rea@ruangguru.com>
2022-09-24 14:34:50 +0700

    add Nodes class for generating child node

---
+++ Nodes.py
@@ -0,0 +1,21 @@
+from GameState import GameState
+
+class Nodes(GameState):
+    def __init__(self, GameState):
+        self.Current = GameState
+        self.CurrentScore = 0
+        self.children = {}
+    
+    def Make(self, i, j, rowcol):
+        childState = self.Current
+        childState.board_status[j,i] = 1
+        if rowcol == "row":
+            childState.row_status[j,i] = 1
+        else:
+            childState.col_status[j,i] = 1
+        childState.player1_turn = not childState.player1_turn
+
+        self.children[(i, j)] = Nodes(childState)
+        
+    def Populate(self, i, j, Child):
+        self.children[(i,j)] = Child
