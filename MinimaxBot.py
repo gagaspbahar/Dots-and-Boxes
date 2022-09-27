9564b64fd004693487752e51bdb442ced99f9736
Ubaidillah Ariq <ubaidillahariq.rea@ruangguru.com>
2022-09-27 13:12:27 +0700

    bot works but idiot

--- MinimaxBot.py
+++ MinimaxBot.py
@@ -10,26 +10,34 @@ class MinimaxBot(Bot):
             for j in range (4) :
                 if state.row_status[j,i] == 1:
                     count += 1
-
         for i in range (4):
             for j in range (3):
                 if state.col_status[j,i] == 1:
                     count += 1
+        count = 24-count
+        # print(count)
+        # print(state.board_status)
+        # print(state.row_status)
+        # print(state.col_status)
+        # print(state.player1_turn)
+        # State = GameState(state.board_status, state.row_status, state.col_status, state.player1_turn)
+        Node = Nodes(state.board_status, state.row_status, state.col_status, state.player1_turn)
+        return self.get_minimax_action(Node, count)
 
-        return self.get_minimax_action(Nodes(state), 24-count)
-
-    def get_minimax_action(Node, Depth):
+    def get_minimax_action(self, Node, Depth):
         for i in range (3):
             for j in range (4):
-                if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.children:
+                if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.Children:
                     Node.Make(i, j, "row")
+                    #print("row", i, j)
                     if Depth < 2 :
                         return GameAction("row", (i,j))
 
         for i in range (4):
             for j in range (3):
-                if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.children:
+                if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.Children:
                     Node.Make(i, j, "col")
+                    #print("col", i, j)
                     if Depth < 2 :
                         return GameAction("col", (i,j))
 
@@ -37,33 +45,40 @@ class MinimaxBot(Bot):
         i = 0
         j = 0
         rowcol = ""
-        for k, z in Nodes.children.items():
-            Result = MinimaxBot.Maximum(z, Depth - 1, Minimum_Score)
+        for k, z in Node.Children.items():
+            Result = self.Maximum(z, Depth - 1, Minimum_Score)
+            # print(k)
+            # print(Result)
             if Minimum_Score > Result:
                 Minimum_Score = Result
                 i = k[0]
                 j = k[1]
                 rowcol = k[2]
 
+        print(rowcol, (i,j))
         return GameAction(rowcol, (i,j))
 
-    def Maximum(Node, Depth, Alpha):
+    def Maximum(self, Node, Depth, Alpha):
         if Depth == 0:
             return Node.CurrentScore
         
         for i in range (3):
             for j in range (4):
-                if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.children:
+                if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.Children:
                     Node.Make(i, j, "row")
+                    print("row", i, j)
 
         for i in range (4):
             for j in range (3):
-                if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.children:
+                if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.Children:
                     Node.Make(i, j, "col")
+                    print("col", i, j)
 
         Maximum_Score = -1000
-        for k, z in Nodes.children.items():
-            Result = MinimaxBot.Minimum(z, Depth - 1, Maximum_Score)
+        for k, z in Node.Children.items():
+            Result = self.Minimum(z, Depth - 1, Maximum_Score)
+            # print(k)
+            # print(Result)
             if Maximum_Score < Result:
                 Maximum_Score = Result
             if Result > Alpha:
@@ -71,23 +86,27 @@ class MinimaxBot(Bot):
         
         return Maximum_Score
 
-    def Minimum(Node, Depth, Beta):
+    def Minimum(self, Node, Depth, Beta):
         if Depth == 0:
             return Node.CurrentScore
         
         for i in range (3):
             for j in range (4):
-                if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.children:
+                if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.Children:
                     Node.Make(i, j, "row")
+                    print("row", i, j)
 
         for i in range (4):
             for j in range (3):
-                if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.children:
+                if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.Children:
                     Node.Make(i, j, "col")
+                    print("col", i, j)
 
         Minimum_Score = 1000
-        for k, z in Nodes.children.items():
-            Result = MinimaxBot.Maximum(z, Depth - 1, Minimum_Score)
+        for k, z in Node.Children.items():
+            Result = self.Maximum(z, Depth - 1, Minimum_Score)
+            # print(k)
+            # print(Result)
             if Minimum_Score > Result:
                 Minimum_Score = Result
             if Result < Beta:
--- Nodes.py
+++ Nodes.py
@@ -2,58 +2,65 @@ from GameState import GameState
 import numpy as np
 
 class Nodes(GameState):
-    def __init__(self, GameState):
-        self.Current = GameState
+    def __init__(self, board_status, row_status, col_status, player1_turn):
+        self.Current = GameState(board_status, row_status, col_status, player1_turn)
         self.CurrentScore = 0
-        self.children = {}
-
-    def update_board(self, x, y, type):
-        val = 1
-        playerModifier = 1
-        pointScored = False
-        if self.player1_turn:
-            playerModifier = -1
-            
-
-        if y < 3 and x < 3:
-            self.board_status[y][x] = (abs(self.board_status[y][x]) + val) * playerModifier
-            if abs(self.board_status[y][x]) == 4:
-                pointScored = True
-
-        if type == 'row':
-            self.row_status[y][x] = 1
-            if y >= 1:
-                self.board_status[y-1][x] = (abs(self.board_status[y-1][x]) + val) * playerModifier
-                if abs(self.board_status[y-1][x]) == 4:
-                    pointScored = True
-
-        elif type == 'col':
-            self.col_status[y][x] = 1
-            if x >= 1:
-                self.board_status[y][x-1] = (abs(self.board_status[y][x-1]) + val) * playerModifier
-                if abs(self.board_status[y][x-1]) == 4:
-                    pointScored = True
-
-        if not pointScored:
-            self.player1_turn = not self.player1_turn
+        self.Children = {}
+        
     
     def Make(self, i, j, rowcol):
         childState = self.Current
-        childState.update_board(i, j, rowcol)
+        val = 1
+        playerModifier = 1
+        pointScored = False
+        nextTurn = True
+        currentScore = 0
+        if childState.player1_turn:
+            playerModifier = -1
+            
 
+        if j < 3 and i < 3:
+            childState.board_status[j][i] = (abs(childState.board_status[j][i]) + val) * playerModifier
+            if abs(childState.board_status[j][i]) == 4:
+                pointScored = True
+
+        if rowcol == 'row':
+            childState.row_status[j][i] = 1
+            if j >= 1:
+                childState.board_status[j-1][i] = (abs(childState.board_status[j-1][i]) + val) * playerModifier
+                if abs(childState.board_status[j-1][i]) == 4:
+                    pointScored = True
+
+        elif rowcol == 'col':
+            childState.col_status[j][i] = 1
+            if i >= 1:
+                childState.board_status[j][i-1] = (abs(childState.board_status[j][i-1]) + val) * playerModifier
+                if abs(childState.board_status[j][i-1]) == 4:
+                    pointScored = True
+
+        if not pointScored:
+            nextTurn = not childState.player1_turn
+        else:
+            nextTurn = childState.player1_turn
+        
         count = 0
-        for i in range (4):
-            for j in range (4) :
-                if childState.row_status[j,i] == 1:
+        for x in range (3):
+            for y in range (4) :
+                if childState.row_status[y,x] == 1:
                     count += 1
-                if childState.col_status[j,i] == 1:
+        for x in range (4):
+            for y in range (3) :
+                if childState.col_status[y,x] == 1:
                     count += 1
+
         if count == 24:
             player1_score = len(np.argwhere(self.board_status == -4))
             player2_score = len(np.argwhere(self.board_status == 4))
-            childState.CurrentScore = player2_score - player1_score
+            currentScore = player2_score - player1_score
+            print(currentScore)
 
-        self.children[(i, j, rowcol)] = Nodes(childState)
+        self.Children[(i, j, rowcol)] = Nodes(childState.board_status, childState.row_status, childState.col_status, nextTurn)
+        self.Children[(i, j, rowcol)].CurrentScore = currentScore
         
     def Populate(self, i, j, rowcol, Child):
-        self.children[(i,j,rowcol)] = Child
+        self.Children[(i,j,rowcol)] = Child
