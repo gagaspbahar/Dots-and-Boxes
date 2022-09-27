580422f233cf917284f42c0f0ac3701041cc30d9
Ubaidillah Ariq <ubaidillahariq.rea@ruangguru.com>
2022-09-27 10:42:21 +0700

    add minimum and maximum function

--- MinimaxBot.py
+++ MinimaxBot.py
@@ -5,16 +5,30 @@ from Nodes import Nodes
 
 class MinimaxBot(Bot):
     def get_action(self, state: GameState) -> GameAction:
-        pass
+        count = 0
+        for i in range (3):
+            for j in range (4) :
+                if state.row_status[j,i] == 1:
+                    count += 1
+
+        for i in range (4):
+            for j in range (3):
+                if state.col_status[j,i] == 1:
+                    count += 1
+
+        return self.get_minimax_action(Nodes(state), 24-count)
 
     def get_minimax_action(Node, Depth):
-        for i in range (4):
+        for i in range (3):
             for j in range (4):
-                if Node.Current.row_status[j,i] == 0 and (i,j) not in Node.children:
+                if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.children:
                     Node.Make(i, j, "row")
                     if Depth < 2 :
                         return GameAction("row", (i,j))
-                if Node.Current.col_status[j,i] == 0 and (i,j) not in Node.children:
+
+        for i in range (4):
+            for j in range (3):
+                if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.children:
                     Node.Make(i, j, "col")
                     if Depth < 2 :
                         return GameAction("col", (i,j))
@@ -34,8 +48,50 @@ class MinimaxBot(Bot):
         return GameAction(rowcol, (i,j))
 
     def Maximum(Node, Depth, Alpha):
-        pass
+        if Depth == 0:
+            return Node.CurrentScore
+        
+        for i in range (3):
+            for j in range (4):
+                if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.children:
+                    Node.Make(i, j, "row")
+
+        for i in range (4):
+            for j in range (3):
+                if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.children:
+                    Node.Make(i, j, "col")
+
+        Maximum_Score = -1000
+        for k, z in Nodes.children.items():
+            Result = MinimaxBot.Minimum(z, Depth - 1, Maximum_Score)
+            if Maximum_Score < Result:
+                Maximum_Score = Result
+            if Result > Alpha:
+                return Result
+        
+        return Maximum_Score
 
     def Minimum(Node, Depth, Beta):
-        pass
+        if Depth == 0:
+            return Node.CurrentScore
+        
+        for i in range (3):
+            for j in range (4):
+                if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.children:
+                    Node.Make(i, j, "row")
+
+        for i in range (4):
+            for j in range (3):
+                if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.children:
+                    Node.Make(i, j, "col")
+
+        Minimum_Score = 1000
+        for k, z in Nodes.children.items():
+            Result = MinimaxBot.Maximum(z, Depth - 1, Minimum_Score)
+            if Minimum_Score > Result:
+                Minimum_Score = Result
+            if Result < Beta:
+                return Result
+        
+        return Minimum_Score
                 
