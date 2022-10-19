469c417999013a1c1de30248ca6555450ddb636e
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2022-10-19 14:36:19 +0700

    Tidy up program (delete comments)

--- MinimaxBot.py
+++ MinimaxBot.py
@@ -20,19 +20,14 @@ class MinimaxBot(Bot):
                     count += 1
         count = 24-count
 
-        # Node = Nodes(state.board_status, state.row_status, state.col_status, state.player1_turn, minimax, currentScore, position)
         Node = Nodes(state.board_status, state.row_status, state.col_status, state.player1_turn)
         return self.get_minimax_action(Node, count, 1)  
     
     def dynamic_depth_limit(self, depth) -> int:
         if depth < 8:
           return 4
-        # if depth == 5:
-        #   return 5
         elif depth < 12:
           return 5
-        # elif depth == 11:
-        #   return 7
         elif depth < 15:
           return 6
         elif depth < 16:
@@ -42,14 +37,10 @@ class MinimaxBot(Bot):
 
 
     def get_minimax_action(self, Node, Height, Depth):
-        start = time()
-        print('==================new action')
         for i in range (3):
             for j in range (4):
                 if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.Positions:
                     Node.Make(i, j, "row", self.player1_turn)
-                    # print("row", i, j)
-                    # print(Node.Children[(i, j, "row")])
                     if Height < 2 :
                         return GameAction("row", (i,j))
 
@@ -57,50 +48,30 @@ class MinimaxBot(Bot):
             for j in range (3):
                 if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.Positions:
                     Node.Make(i, j, "col", self.player1_turn)
-                    # print("col", i, j)
-                    # print(Node.Children[(i, j, "col")])
                     if Height < 2 :
                         return GameAction("col", (i,j))
 
         MaxScore = -1000
-        # MinScore = 1000
         i = 0
         j = 0
         rowcol = ""
         initialDepthLimit = self.dynamic_depth_limit(24-Height)
-        print("depth: ", initialDepthLimit)
-        print("turn: ", 24-Height)
-        # for _, z in Node.Children.items():
         for _ in range(len(Node.Children)):
-            # Result = self.Maximum(z, Height - 1, Minimum_Score, Depth+1)
             data = heapq.heappop(Node.Children)
             k = data.Position
             z = data
             Result = self.Minimum(z, Height - 1, MaxScore, Depth+1, initialDepthLimit)
             print(k, " ", Result)
-            # if Minimum_Score > Result:
-            #     Minimum_Score = Result
-            #     i = k[0]
-            #     j = k[1]
-            #     rowcol = k[2]
             if MaxScore < Result:
                 MaxScore = Result
                 i = k[0]
                 j = k[1]
                 rowcol = k[2]
-            # print("min: ", Minimum_Score)
-            print("max: ", MaxScore)
-            print("treeline", data.ScoreWithThreeline)
 
-        # print(rowcol, (i,j))
-        print("timetaken:", time() - start)
-        print("taken: ", rowcol, (i,j))
         return GameAction(rowcol, (i,j))
 
     def Maximum(self, Node, Height, Alpha, Depth, DepthLimit):
-        # print(Height)
         if Height == 0 or Depth == DepthLimit:
-            # print(Node.CurrentScore)
             return Node.CurrentScore
         
         for i in range (3):
@@ -132,27 +103,18 @@ class MinimaxBot(Bot):
         return Maximum_Score
 
     def Minimum(self, Node, Height, Beta, Depth, DepthLimit):
-        # print(Height)
         if Height == 0 or Depth == DepthLimit:
-            # print(Node.CurrentScore)
             return Node.CurrentScore
-
-        # if Depth == 5:
-        #     player1_score = len(np.argwhere(Node.board_status == -4))
-        #     player2_score = len(np.argwhere(Node.board_status == 4))
-        #     return player2_score - player1_score
         
         for i in range (3):
             for j in range (4):
                 if Node.Current.row_status[j,i] == 0 and (i,j,"row") not in Node.Positions:
                     Node.Make(i, j, "row", self.player1_turn)
-                    # print("row", i, j)
 
         for i in range (4):
             for j in range (3):
                 if Node.Current.col_status[j,i] == 0 and (i,j,"col") not in Node.Positions:
                     Node.Make(i, j, "col", self.player1_turn)
-                    # print("col", i, j)
                     
         Maximum_Score = -1000
         Minimum_Score = 1000
--- Nodes.py
+++ Nodes.py
@@ -16,11 +16,7 @@ class Nodes():
     
     def __lt__(self, other):
         return self.ScoreWithThreeline > other.ScoreWithThreeline
-        # if not self.Minimax:
-        #     return self.ScoreWithThreeline < other.ScoreWithThreeline
-        # else:
-        #     return self.ScoreWithThreeline > other.ScoreWithThreeline
-    
+
     def Make(self, i, j, rowcol, myTurn):
         childState = deepcopy(self.Current)
         playerModifier = 1
@@ -57,31 +53,6 @@ class Nodes():
             nextTurn = childState.player1_turn
         
 
-        # count = 0
-        # for x in range (3):
-        #     for y in range (4) :
-        #         if childState.row_status[y,x] == 1:
-        #             count += 1
-        # for x in range (4):
-        #     for y in range (3) :
-        #         if childState.col_status[y,x] == 1:
-        #             count += 1
-
-        # TURN = FALSE
-        # if minimaxBot == player2 -> TURN = FALSE
-        # if minimaxBot == player1 -> TURN = TRUE
-        # if not myTurn:
-        #     # if minimax bot player2
-        #     initial_player_score = len(argwhere(self.Current.board_status == 4))
-        #     enemy_score = len(argwhere(childState.board_status == -4))
-        #     player_score = len(argwhere(childState.board_status == 4))
-            
-        # else:
-        #     # if minimax bot player1
-        #     initial_player_score = len(argwhere(self.Current.board_status == -4))
-        #     enemy_score = len(argwhere(childState.board_status == 4))
-        #     player_score = len(argwhere(childState.board_status == -4))
-
         # if minimax bot player2
         initial_player_score = len(argwhere(self.Current.board_status == 4))
         enemy_score = len(argwhere(childState.board_status == -4))
@@ -92,9 +63,6 @@ class Nodes():
 
         delta_player_score = player_score - initial_player_score
 
-
-        
-
         if childState.player1_turn != myTurn:
             newThreeLine = len(argwhere(abs(childState.board_status) == 3))
             scoreWithThreeLine = delta_player_score * 40 + 10*(newThreeLine - threeLine)
@@ -102,16 +70,8 @@ class Nodes():
             newThreeLine = len(argwhere(abs(childState.board_status) == 3))
             scoreWithThreeLine = delta_player_score * 40 - 10*(newThreeLine - threeLine)
 
-        # self.Children[self.encode(i,j,rowcol)] = Nodes(childState.board_status, childState.row_status, childState.col_status, nextTurn)
-        # self.Children[self.encode(i, j, rowcol)].CurrentScore = currentScore
-        # (pq_score, (i,j,rowcol))
-        # (pq_score, Nodes)
-        # self.Children.append(Nodes(childState.board_status, childState.row_status, childState.col_status, nextTurn, currentScore, (i,j,rowcol)))
         if nextTurn:
             heapq.heappush(self.Children, Nodes(childState.board_status, childState.row_status, childState.col_status, nextTurn, self.Minimax, currentScore, (i,j,rowcol), scoreWithThreeLine))
         else:
             heapq.heappush(self.Children, Nodes(childState.board_status, childState.row_status, childState.col_status, nextTurn, not self.Minimax, currentScore, (i,j,rowcol), scoreWithThreeLine))
         self.Positions.append((i,j,rowcol))
-
-    # def Populate(self, i, j, rowcol, Child):
-    #     self.Children[self.encode(i,j,rowcol)] = Child
